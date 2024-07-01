154bfa0a7fb837aeb55d934362e680c31dbab06f
Fsepehrpour <127209196+Fsepehrpour@users.noreply.github.com>
2024-07-01 15:56:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 class Power():
   """ A class that computes a specific power of other numbers.
-  in other words, it raises a number by a contant exponent."""
+  in other words, it raises a number by a constant exponent."""
 
-  default_exponent = 2
-  def __init__(self, exponent = default_exponent):
+  def __init__(self, exponent):
     self.exponent = exponent
 
   def of(self, x):
     return x ** self.exponent
 
 class RealPower(Power):
-  """ A subcalss of Power for real numbers."""
+  """ A subclass of Power for real numbers."""
 
 def of(self, x):
   if isinstance(self.exponent, int) or x>=0:
@@ -18,3 +17,13 @@ def of(self, x):
   raise ValueError(
     "Fractional powers of negative numbers are imaginary" 
   )
+
+square = Power(2)
+root = Power(0.5)
+print("square: ", square)
+print("square.of(3) =", square.of(3))
+print("root.of(3) =", root.of(3))
+print("root.of(-3) =", root.of(-3))
+real_root = RealPower(0.5)
+print("real_root.of(3) =", real_root.of(3))
+print("real_root.of(-3) =", real_root.of(-3))
